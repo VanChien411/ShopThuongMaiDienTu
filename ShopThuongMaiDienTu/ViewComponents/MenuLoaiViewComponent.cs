29654c0f292f584af437063f748d774861ab2509
Your Name <your_email@example.com>
2024-10-11 21:29:20 +0700

    Create viewComponents, view , component def

---
+++ ShopThuongMaiDienTu/ViewComponents/MenuLoaiViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopThuongMaiDienTu.Data;
+using ShopThuongMaiDienTu.ViewModels;
+
+namespace ShopThuongMaiDienTu.ViewComponents
+{
+    public class MenuLoaiViewComponent:ViewComponent
+    {
+        private readonly ShopThuongMaiDienTuContext _context;
+        public MenuLoaiViewComponent(ShopThuongMaiDienTuContext context) { 
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var data = _context.Loais.Select(x => new MenuLoaiVM
+            {
+               MaLoai = x.MaLoai,
+               TenLoai = x.TenLoai,
+                SoLuong = x.HangHoas.Count()
+            }).OrderBy(o => o.TenLoai);
+            return View(data);
+        }
+    }
+}
