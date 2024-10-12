ae025d2faff6e9e7e7484c3b40acd6cdd5eb1d97
VanChien411 <phuthienchien3@gmail.com>
2024-10-12 11:23:35 +0700

    update img and information for item

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ShopThuongMaiDienTu
+
+Tải hình về giải nén và đưa vào \ShopThuongMaiDienTu\wwwroot
+https://drive.google.com/file/d/1KWuhUEjbl2ujGP-LqVqEshQ49d-jdggr/view?usp=sharing
--- ShopThuongMaiDienTu/Controllers/HangHoaController.cs
+++ ShopThuongMaiDienTu/Controllers/HangHoaController.cs
@@ -1,12 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShopThuongMaiDienTu.Data;
+using ShopThuongMaiDienTu.ViewModels;
 
 namespace ShopThuongMaiDienTu.Controllers
 {
     public class HangHoaController : Controller
     {
+        private readonly ShopThuongMaiDienTuContext _context;
+
+        public HangHoaController(ShopThuongMaiDienTuContext context) { 
+            _context = context;
+        }
         public IActionResult Index(int? loai)
         {
-            return View();
+            var hangHoas = _context.HangHoas.AsQueryable();
+
+            if (loai.HasValue) { 
+                hangHoas = hangHoas.Where(x => x.MaLoai == loai.Value);
+            }
+
+            var result = hangHoas.Select(x => new HangHoaVM
+            {
+                MaHangHoa = x.MaHh,
+                TenHangHoa = x.TenHh,
+                DonGia = x.DonGia ?? 0,
+                Hinh = x.Hinh ?? "",
+                MoTaNgan = x.MoTaDonVi ?? "",
+                TenLoai = x.MaLoaiNavigation.TenLoai
+            });
+            return View(result);
         }
     }
 }
