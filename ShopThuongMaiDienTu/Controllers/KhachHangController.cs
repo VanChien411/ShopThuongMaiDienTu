291a63a3819c52244c79e79682287aac446211ff
VanChien411 <phuthienchien3@gmail.com>
2024-10-13 17:48:10 +0700

    Create register

---
+++ ShopThuongMaiDienTu/Controllers/KhachHangController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopThuongMaiDienTu.Data;
+using ShopThuongMaiDienTu.ViewModels;
+
+namespace ShopThuongMaiDienTu.Controllers
+{
+    public class KhachHangController : Controller
+    {
+        private readonly ShopThuongMaiDienTuContext _context;
+
+        public KhachHangController(ShopThuongMaiDienTuContext context) {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult DangKy()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult DangKy(RegisterVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                var khachHang = model;
+            }
+            return View();
+        }
+        public IActionResult DangNhap()
+        {
+            return View();
+        }
+    }
+}
---
+++ ShopThuongMaiDienTu/ViewModels/RegisterVM.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShopThuongMaiDienTu.ViewModels
+{
+    public class RegisterVM
+    {
+        [Display(Name = "Tên đăng nhập")]
+        [Required(ErrorMessage ="*")]
+        [MaxLength(20, ErrorMessage ="Tối đa 20 ký tự")]
+        public string MaKh { get; set; }
+        [Display(Name = "Mật khẩu")]
+        [Required(ErrorMessage ="*")]
+        public string MatKhau { get; set; }
+        [Display(Name = "Họ tên")]
+        [Required(ErrorMessage = "*")]
+        [MaxLength(50, ErrorMessage ="Tối đa 50 ký tự")]
+        public string HoTen { get; set; }
+
+        public bool? GioiTinh { get; set; } = true;
+
+        public DateTime? NgaySinh { get; set; }
+        [MaxLength(70, ErrorMessage = "Tối đa 70 ký tự")]
+        public string? DiaChi { get; set; }
+        [MaxLength(30, ErrorMessage = "Tối đa 30 ký tự")]
+        [RegularExpression(@"0[9875]\d{8}", ErrorMessage ="Chưa đúng định dạng điện thoại")]
+        public string? DienThoai { get; set; }
+        [EmailAddress(ErrorMessage ="Chưa đứng định dạng email")]
+        public string Email { get; set; }
+
+        public string? Hinh { get; set; }
+
+   
+    }
+}
