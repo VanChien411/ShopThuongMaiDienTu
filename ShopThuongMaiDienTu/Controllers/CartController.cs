f3de802b8537cfabf432fbc551313e79aee14ad7
VanChien411 <phuthienchien3@gmail.com>
2024-10-16 00:09:24 +0700

    create information checkout and save ..

--- ShopThuongMaiDienTu/Controllers/CartController.cs
+++ ShopThuongMaiDienTu/Controllers/CartController.cs
@@ -3,6 +3,7 @@ using ShopThuongMaiDienTu.Data;
 using ShopThuongMaiDienTu.ViewModels;
 using ShopThuongMaiDienTu.Helpers;
 using Microsoft.AspNetCore.Authorization;
+using ShopThuongMaiDienTu.Models;
 
 namespace ShopThuongMaiDienTu.Controllers
 {
@@ -102,11 +103,68 @@ namespace ShopThuongMaiDienTu.Controllers
             return RedirectToAction("index");
         }
         [Authorize]
+        [HttpGet]
         public IActionResult CheckOut()
         {
            if(Cart.Count == 0)
             {
                 return Redirect("/");
+            }
+            return View(Cart);
+        }
+        [Authorize]
+        [HttpPost]
+        public IActionResult CheckOut(CheckoutVM model)
+        {
+            if (ModelState.IsValid) {
+                var customerId = HttpContext.User.Claims.SingleOrDefault(x => x.Type == MySetting.CLAIM_CUSTOMERID).Value;
+                var khachHang = new KhachHang();
+                if (model.GiongKhachHang)
+                {
+                    khachHang = _context.KhachHangs.SingleOrDefault(x => x.MaKh == customerId);
+                }
+
+                var hoadon = new HoaDon
+                {
+                    MaKh = customerId,
+                    HoTen = model.HoTen ?? khachHang.HoTen,
+                    DiaChi = model.DiaChi ?? khachHang.DiaChi,
+                    NgayDat = DateTime.Now,
+                    CachThanhToan = "COD",
+                    CachVanChuyen = "Grab",
+                    MaTrangThai = 0,
+                    GhiChu = model.GhiChu
+                };
+                _context.Database.BeginTransaction();
+                try
+                {
+                   
+                    _context.Database.CommitTransaction();
+                    _context.Add(hoadon);
+                    _context.SaveChanges();
+
+                    var cthd = new List<ChiTietHd>();
+                    foreach(var item in Cart)
+                    {
+                        cthd.Add(new ChiTietHd
+                        {
+                            MaHd = hoadon.MaHd,
+                            SoLuong = item.SoLuong,
+                            DonGia = item.DonGia,
+                            MaHh = item.MaHangHoa,
+                            GiamGia = 0
+                        });
+                        _context.SaveChanges();
+                        HttpContext.Session.Set<List<CartItem>>(MySetting.CART_KEY, new List<CartItem>());
+
+                        return View("Success");
+                    }
+                }
+                catch
+                {
+                    _context.Database.RollbackTransaction();
+                }
+             
             }
             return View(Cart);
         }
--- ShopThuongMaiDienTu/Controllers/KhachHangController.cs
+++ ShopThuongMaiDienTu/Controllers/KhachHangController.cs
@@ -97,7 +97,7 @@ namespace ShopThuongMaiDienTu.Controllers
                             {
                                 new Claim(ClaimTypes.Email, khachHang.Email),
                                 new Claim(ClaimTypes.Name, khachHang.HoTen),
-                                new Claim("CustomerID", khachHang.MaKh),
+                                new Claim(MySetting.CLAIM_CUSTOMERID, khachHang.MaKh),
                                 new Claim(ClaimTypes.Role, "Customer")
                             };
                             
